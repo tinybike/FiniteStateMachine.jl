234b7b681a414a4517760ad4cae98e1059d9fcac
Jack Peterson <jack@tinybike.net>
2014-07-11 22:44:01 -0700

    event/state/callback layout

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# FiniteStateMachine
+## FiniteStateMachine
+
+Finite state machine library for Julia, based on Jake Gordon's excellent [javascript-state-machine](https://github.com/jakesgordon/javascript-state-machine).
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,14 @@
 using FiniteStateMachine
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+fsm = StateMachine(
+    "initialstate",
+    ["state1", "state2", "state3"],
+    ["event1", "event2"],
+    ["callback1", "callback2"]
+)
+
+@test fsm.initial == "initialstate"
+@test fsm.states == ["state1", "state2", "state3"]
+@test fsm.events == ["event1", "event2"]
+@test fsm.callbacks == ["callback1", "callback2"]
